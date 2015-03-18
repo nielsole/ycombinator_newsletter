5345be4ff11fc559295a6705d05700c57f24abbd
Niels-Ole Kühl <nielsole.kuehl@gmail.com>
2015-03-18 16:16:58 +0100

    Ein @ Zeichen fehlte

--- midnight.py
+++ midnight.py
@@ -8,12 +8,14 @@ from credentials import API_KEY, HOST
 __author__ = 'flshrmb'
 
 
-def send_simple_message(some_list,address):
+def send_simple_message(some_list):
+    with open('addresses.txt') as f:
+        addresses = f.read().splitlines()
     return requests.post(
-        "https://api.mailgun.net/v2/" + HOST + "/messages",
+        "https://api.mailgun.net/v3/" + HOST + "/messages",
         auth=("api", API_KEY),
-        data={"from": u"Hacker News - Täglicher Report <mailgun" + HOST + ">",
-              "to": address,
+        data={"from": u"Hacker News - Täglicher Report <mailgun@" + HOST + ">",
+              "to": addresses,
               "subject": "Hacker News Update",
               "html": u'<html>{0}<br>To be removed from this mailing list please contact nielsole.kuehl@gmail.com</html>'.format(some_list)})
 
@@ -22,10 +24,7 @@ def main():
     message = "Top messages:<br>"
     for story in db.all():
         message += u'{0} <a href="{1}">{2}</a><br>'.format(story['score'], story['url'], cgi.escape(story['title']))
-    with open('addresses.txt') as f:
-        addresses = f.read().splitlines()
-    for address in addresses:
-        send_simple_message(message, address)
+    result = send_simple_message(message)
     os.remove('db.json')
 
 
