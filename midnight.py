2fbb32594cfb0c51c5322426c0866bab7db1204d
Niels-Ole Kühl <nielsole.kuehl@gmail.com>
2015-03-20 11:16:22 +0100

    *Comments link *Custom user

--- midnight.py
+++ midnight.py
@@ -4,7 +4,7 @@ import os
 import requests
 from tinydb import TinyDB
 import cgi
-from credentials import API_KEY, HOST
+from credentials import API_KEY, USER, HOST
 __author__ = 'flshrmb'
 
 
@@ -14,18 +14,18 @@ def send_simple_message(some_list):
     return requests.post(
         "https://api.mailgun.net/v3/" + HOST + "/messages",
         auth=("api", API_KEY),
-        data={"from": u"Hacker News - Täglicher Report <mailgun@" + HOST + ">",
+        data={"from": u"Hacker News - Täglicher Report <{0}@{1}>".format(USER, HOST),
               "to": addresses,
               "subject": "Hacker News Update",
-              "html": u'<html>{0}<br>To be removed from this mailing list please contact nielsole.kuehl@gmail.com</html>'.format(some_list)})
+              "html": u'<html>{0}<br>To be removed from this mailing list write an email with the subject \'unsubscribe\' to hackernewsletter-request@freelists.org</html>'.format(some_list)})
 
 def main():
     db = TinyDB('db.json')
     message = "Top messages:<br>"
     for story in db.all():
-        message += u'{0} <a href="{1}">{2}</a><br>'.format(story['score'], story['url'], cgi.escape(story['title']))
+        message += u'{0} <a href="{1}">{2}</a> (<a href="https://news.ycombinator.com/item?id={3}">Comments</a>)<br>'.format(story['score'], story['url'], cgi.escape(story['title']), story['id'])
     result = send_simple_message(message)
-    os.remove('db.json')
+    #os.remove('db.json')
 
 
 if __name__ == "__main__":
